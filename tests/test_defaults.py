3ee90baa871a5608b6204369c8de5561b9c0ce4a
Jakub Rozek <jrozek@redhat.com>
2023-11-17 17:57:34 +0100

    Add positional tests

--- tests/test_defaults.py
+++ tests/test_defaults.py
@@ -1,28 +1,27 @@
 from typing import Any
 import pytest
-from arcparse import ArcParser, positional, flag, no_flag, subparsers
+
+from arcparse import ArcParser, positional, option, flag, no_flag
 
 
-class ReqArgs(ArcParser):
-    positional_int: int = positional()
-
-
-class AllArgs(ArcParser):
+class OptArgs(ArcParser):
     optional_str: str | None
     optional_int: int | None
+    optional_str_alt: str | None = option()
     default_str: str = "foo"
+    default_str_alt: str = option(default="foo")
     default_int: int = 123
     implicit_flag: bool
     explicit_flag: bool = flag()
     no_flag: bool = no_flag()
 
-    action: ReqArgs | None = subparsers("")
-
 
 defaults = {
     "optional_str": None,
     "optional_int": None,
+    "optional_str_alt": None,
     "default_str": "foo",
+    "default_str_alt": "foo",
     "default_int": 123,
     "implicit_flag": False,
     "explicit_flag": False,
@@ -31,7 +30,7 @@ defaults = {
 
 
 def test_defaults_sanity() -> None:
-    parsed = AllArgs.parse([])
+    parsed = OptArgs.parse([])
     for key, value in defaults.items():
         assert getattr(parsed, key) == value
 
@@ -41,7 +40,9 @@ def test_defaults_sanity() -> None:
     [
         ("--optional-str bar", "optional_str", "bar"),
         ("--optional-int 456", "optional_int", 456),
-        ("--default-str baz", "default_str", "baz"),
+        ("--optional-str-alt bar", "optional_str_alt", "bar"),
+        ("--default-str bar", "default_str", "bar"),
+        ("--default-str bar", "default_str", "bar"),
         ("--default-int 789", "default_int", 789),
         ("--implicit-flag", "implicit_flag", True),
         ("--explicit-flag", "explicit_flag", True),
@@ -50,7 +51,28 @@ def test_defaults_sanity() -> None:
 )
 def test_defaults(arg_string: str, key: str, value: Any) -> None:
     args = arg_string.split()
-    parsed = AllArgs.parse(args)
+    parsed = OptArgs.parse(args)
 
     for k, v in (defaults | {key: value}).items():
+        assert getattr(parsed, k) == v
+
+
+class PosArgs(ArcParser):
+    positional_str: str = positional()
+    positional_int: int = positional()
+    positional_default_str: str | None = positional()
+
+
+@pytest.mark.parametrize(
+    "arg_string,values",
+    [
+        ("foo 123", {"positional_str": "foo", "positional_int": 123, "positional_default_str": None}),
+        ("foo 123 bar", {"positional_str": "foo", "positional_int": 123, "positional_default_str": "bar"}),
+    ],
+)
+def test_positional_defaults(arg_string: str, values: dict[str, Any]) -> None:
+    args = arg_string.split()
+    parsed = PosArgs.parse(args)
+
+    for k, v in values.items():
         assert getattr(parsed, k) == v
