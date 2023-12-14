73261b1535d8d05074c3dda1e5fe7a8fc6926a38
Jakub Rozek <jrozek@redhat.com>
2023-12-14 12:22:09 +0100

    isort: Ensure 2 lines after imports

--- arcparse/__init__.py
+++ arcparse/__init__.py
@@ -2,6 +2,7 @@ from ._arguments import MxGroup, flag, no_flag, option, positional
 from ._parser import arcparser, subparsers
 from .converters import itemwise
 
+
 __all__ = [
     "arcparser",
     "positional",
