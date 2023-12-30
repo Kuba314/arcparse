36580cc883569e0ae6befef734539cf80d852c7e
Jakub Rozek <jrozek@redhat.com>
2023-12-30 16:50:05 +0100

    Fix name_override in option

--- arcparse/_argument_helpers.py
+++ arcparse/_argument_helpers.py
@@ -52,6 +52,9 @@ def option[T](
     if short_only and short is None:
         raise ValueError("`short_only` cannot be True if `short` is not provided")
 
+    if short_only and name_override is not None:
+        raise ValueError("`short_only` cannot be True if `name_override` is provided")
+
     if append and at_least_one:
         raise ValueError("`append` is incompatible with `at_least_one`")
 
